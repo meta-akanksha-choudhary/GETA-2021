4a389480d22b677e7fbbd6af55e02e63fc5591e5
Akanksha Choudhary <akanksha.choudhary@metacube.com>
2021-03-02 19:31:37 +0530

    assignment

--- Recursion/TestRecursion.java
+++ Recursion/TestRecursion.java
@@ -7,18 +7,22 @@ public class TestRecursion {
 		assertEquals(1,Recursion.gcd(5, 3));
 		assertNotEquals(2,Recursion.gcd(15, 3));
 	}
+	@Test
 	public void testLcm(){
 		assertEquals(15,Recursion.lcm(5, 3));
 		assertNotEquals(2,Recursion.lcm(15, 3));
 	}
+	@Test
 	public void testLinearSearch(){
 		assertEquals(3,Recursion.linearSearch(new int[]{3,5,8,6,4},0,4,6));
 		assertNotEquals(3,Recursion.linearSearch(new int[]{3,5,8,6,4},0,4,8));
 	}
+	@Test
 	public void testBinarySearch(){
 		assertEquals(-1,Recursion.binarySearch(new int[]{3,5,8,10,12},0,4,6));
 		assertNotEquals(3,Recursion.binarySearch(new int[]{3,5,8,10,12},0,4,8));
 	}
+	@Test
 	public void testNQueens(){
 		NQueens queen=new NQueens();
 		assertEquals(true,queen.solveNQ(4));
