465c7412a7d633ff2e825f5df9eaea36954f749c
Akanksha Choudhary <akanksha.choudhary@metacube.com>
2021-03-08 21:18:29 +0530

    assignments

---
+++ Collections/sortByName.java
@@ -0,0 +1,8 @@
+
+import java.util.Comparator;
+public class sortByName implements Comparator<Employee>{
+	public int compare(Employee a, Employee b){
+		return (a.getName().compareTo(b.getName()));
+	}
+
+}
---
+++ Collections/sortNatural.java
@@ -0,0 +1,8 @@
+
+import java.util.Comparator;
+public class sortNatural implements Comparator<Employee>{
+	public int compare(Employee a, Employee b){
+		return a.getId()-b.getId();
+	}
+
+}
