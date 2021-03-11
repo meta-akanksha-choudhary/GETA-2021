1c8b19d48c1a63d3eaabb4b376dc2fcdd8ea09ba
Akanksha Choudhary <akanksha.choudhary@metacube.com>
2021-03-11 15:07:52 +0530

    assignment

---
+++ Algorithm 1/Node.java
@@ -0,0 +1,19 @@
+
+public class Node{
+		String key;
+		String value;
+		Node left,right;
+		public Node(String key,String value){
+			this.key=key;
+			this.value=value;
+			left=right=null;
+		}
+		public String getKey(){
+			return this.key;
+			
+		}
+		public String getValue(){
+			return this.value;
+			
+		}
+	}
